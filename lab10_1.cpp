91061ac046645facd62b7ed10d3925eccd4a1ba4
prawsopit <prawsopit@gmail.com>
2022-12-29 13:26:38 +0700

    10.1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double loan , rate , pay;
 	cout << "Enter initial loan: ";
+	cin >> loan ;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate ;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay ;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -18,16 +22,22 @@ int main(){
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
+	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	for(int i =1;loan > 0 ;i+=1){
+	double interest = loan * (rate/100);
+	if((loan + interest) < pay){
+		pay = loan + interest;}
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+	cout << setw(13) << left << i; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << interest;
+	cout << setw(13) << left << loan + interest;
+	cout << setw(13) << left << pay;
+	cout << setw(13) << left << (loan + interest)-pay;
+	cout << "\n";
+	loan = (loan + interest)-pay;
+	}
 	return 0;
 }
